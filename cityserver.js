b5631acf2f6d55ec56656f1de573c34c41d988c7
willvdb <willvdb9141@gmail.com>
2015-03-04 15:45:51 -0700

    changed it to return an empty array for the empty string, and added vdmovies hompage routing

--- cityserver.js
+++ cityserver.js
@@ -3,18 +3,25 @@ var http = require('http');
 var url = require('url');
 var ROOT_DIR = "prod/vdbMovies";
 
+var port = process.argv[2];
+
+function isEmpty(str) {
+  return (!str || 0 === str.length);
+}
+
 
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
   if (urlObj.pathname.indexOf("getcity") !== -1){
     fs.readFile("cities.dat.txt", function(err, data){
       if(err){
-
+        res.writeHead(200);
+        res.end("There was an error getting your cities...")
       } else {
-        if (urlObj["q"] !=== ""){
+        var resultArray = [];
+        if (!isEmpty(urlObj.query["q"])){
           var cities = data.toString().split('\n');
           var myRe = new RegExp("^"+urlObj.query["q"]);
-          var resultArray = [];
           for (i = 0; i < cities.length; i++){
             var result = cities[i].search(myRe);
             if(result != -1) {
@@ -22,10 +29,20 @@ http.createServer(function (req, res) {
               resultArray.push(obj);
             }
           }
-          res.writeHead(200);
-          res.end(JSON.stringify(resultArray));
         }
+        res.writeHead(200);
+        res.end(JSON.stringify(resultArray));
       }
+    });
+  } else if(urlObj.pathname === "/"){
+    fs.readFile("prod/vdbMovies/vdm.htm", function (err,data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
     });
   } else {
     fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
@@ -38,4 +55,4 @@ http.createServer(function (req, res) {
       res.end(data);
     });
   }
-}).listen(8080);
+}).listen(port);
